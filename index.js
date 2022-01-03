0413eaba75214c7a475f07521b7eb78c12a4cbad
Klay Clarke <klayaclarke@gmail.com>
2022-01-02 19:20:06 -0500

    add put and delete routes for salon edit and deletion functionality

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 const Salon = require("./models/salon");
 
 mongoose.connect("mongodb://localhost:27017/cuts-n-dye");
@@ -17,19 +18,11 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
   res.render("home");
 });
-
-// app.get("/addsalon", async (req, res) => {
-//   const shop = new Salon({
-//     title: "Better Cut",
-//     description: "Stylish cuts and stimulating conversation",
-//   });
-//   await shop.save();
-//   res.send(shop);
-// });
 
 app.get("/salons", async (req, res) => {
   const salons = await Salon.find({});
@@ -45,10 +38,27 @@ app.get("/salons/:id", async (req, res) => {
   res.render("salons/show", { salon });
 });
 
+app.get("/salons/:id/edit", async (req, res) => {
+  const salon = await Salon.findById(req.params.id);
+  res.render("salons/edit", { salon });
+});
+
 app.post("/salons", async (req, res) => {
   const salon = new Salon(req.body.salon);
   await salon.save();
   res.redirect(`/salons/${salon._id}`);
+});
+
+app.put("/salons/:id", async (req, res) => {
+  const { id } = req.params;
+  const salon = await Salon.findByIdAndUpdate(id, { ...req.body.salon });
+  res.redirect(`/salons/${salon._id}`);
+});
+
+app.delete("/salons/:id", async (req, res) => {
+  const { id } = req.params;
+  await Salon.findByIdAndDelete(id);
+  res.redirect("/salons");
 });
 
 app.listen(3000, () => {
