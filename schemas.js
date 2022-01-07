ac618d0159f998f67c07b9fca409125c175f005b
Klay Clarke <klayaclarke@gmail.com>
2022-01-06 20:35:59 -0500

    add new params to the joi objects for validation when creating / adding salon to db

--- schemas.js
+++ schemas.js
@@ -3,8 +3,14 @@ const joi = require("joi");
 module.exports.salonValidatorSchema = joi.object({
   salon: joi
     .object({
-      title: joi.string().required(),
-      description: joi.string().required(),
+      salonName: joi.string().required(),
+      salonType: joi.string().required(),
+      salonAveragePrice: joi.number().min(0).required(),
+      salonImage: joi.string().allow(""),
+      salonStreetAddress: joi.string().required(),
+      salonZipCode: joi.number().required(),
+      salonCity: joi.string().required(),
+      salonState: joi.string().required(),
     })
     .required(),
 });
