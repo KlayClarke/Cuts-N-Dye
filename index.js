99481950c2c46e41de1bc73e9e9129ee740bbfb7
Klay Clarke <klayaclarke@gmail.com>
2022-01-10 17:41:56 -0500

    remove app routes from index file - clean up file

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const express = require("express");
+const salonRoutes = require("./routes/salons");
 const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
-const { salonValidatorSchema, reviewValidatorSchema } = require("./schemas");
-const catchAsync = require("./utilities/CatchAsync");
+
 const ExpressError = require("./utilities/ExpressError");
-const Salon = require("./models/salon");
-const Review = require("./models/review");
 
 mongoose.connect("mongodb://localhost:27017/cuts-n-dye");
 
@@ -27,110 +25,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
 
-const validateSalon = (req, res, next) => {
-  const { error } = salonValidatorSchema.validate(req.body);
-  if (error) {
-    const msg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(msg, 400);
-  } else {
-    next();
-  }
-};
-
-const validateReview = (req, res, next) => {
-  const { error } = reviewValidatorSchema.validate(req.body);
-  if (error) {
-    const msg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(msg, 400);
-  } else {
-    next();
-  }
-};
-
 app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.get(
-  "/salons",
-  catchAsync(async (req, res) => {
-    const salons = await Salon.find({});
-    res.render("salons/index", { salons });
-  })
-);
-
-app.get("/salons/new", (req, res) => {
-  res.render("salons/new");
-});
-
-app.get(
-  "/salons/:id",
-  catchAsync(async (req, res, next) => {
-    const salon = await Salon.findById(req.params.id).populate("salonReviews");
-    console.log(salon);
-    res.render("salons/show", { salon });
-  })
-);
-
-app.get(
-  "/salons/:id/edit",
-  catchAsync(async (req, res, next) => {
-    const salon = await Salon.findById(req.params.id);
-    res.render("salons/edit", { salon });
-  })
-);
-
-app.post(
-  "/salons",
-  validateSalon,
-  catchAsync(async (req, res, next) => {
-    const salon = new Salon(req.body.salon);
-    await salon.save();
-    res.redirect(`/salons/${salon._id}`);
-  })
-);
-
-app.post(
-  "/salons/:id/reviews",
-  validateReview,
-  catchAsync(async (req, res) => {
-    const salon = await Salon.findById(req.params.id);
-    const review = new Review(req.body.review);
-    salon.salonReviews.push(review);
-    await review.save();
-    await salon.save();
-    res.redirect(`/salons/${salon._id}`);
-  })
-);
-
-app.put(
-  "/salons/:id",
-  validateSalon,
-  catchAsync(async (req, res, next) => {
-    const { id } = req.params;
-    const salon = await Salon.findByIdAndUpdate(id, { ...req.body.salon });
-    res.redirect(`/salons/${salon._id}`);
-  })
-);
-
-app.delete(
-  "/salons/:id",
-  catchAsync(async (req, res, next) => {
-    const { id } = req.params;
-    await Salon.findByIdAndDelete(id);
-    res.redirect("/salons");
-  })
-);
-
-app.delete(
-  "/salons/:id/reviews/:reviewId",
-  catchAsync(async (req, res, next) => {
-    const { id, reviewId } = req.params;
-    Salon.findByIdAndUpdate(id, { $pull: { salonReviews: reviewId } });
-    const review = await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/salons/${id}`);
-  })
-);
+app.use("/salons", salonRoutes);
 
 app.all("*", (req, res, next) => {
   next(new ExpressError("Page Not Found", 404));
