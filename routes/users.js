94e9e129c27e27f8f1d617c7841b3359e7d3b5c5
Klay Clarke <klayaclarke@gmail.com>
2022-01-14 11:46:06 -0500

    when user registers new account, login user with new account creds using req.login func

--- routes/users.js
+++ routes/users.js
@@ -11,13 +11,16 @@ router.get("/register", (req, res) => {
 
 router.post(
   "/register",
-  catchAsync(async (req, res) => {
+  catchAsync(async (req, res, next) => {
     try {
       const { email, username, password } = req.body;
       const user = new User({ email, username });
       const registeredUser = await User.register(user, password);
-      req.flash("success", "Welcome to Cuts-N-Dye");
-      res.redirect("/salons");
+      req.login(registeredUser, (err) => {
+        if (err) return next(err);
+        req.flash("success", "Welcome to Cuts-N-Dye");
+        res.redirect("/salons");
+      });
     } catch (e) {
       req.flash("error", e.message);
       res.redirect("/register");
