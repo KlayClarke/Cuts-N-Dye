14a373c92a586d8ef483463f6329000786f200b9
Klay Clarke <klayaclarke@gmail.com>
2022-01-15 00:58:25 -0500

    add isAuthor middleware to salon edit form get route, salon edit form post route and salon delete route

--- routes/salons.js
+++ routes/salons.js
@@ -5,7 +5,7 @@ const Salon = require("../models/salon");
 const Review = require("../models/review");
 const { salonValidatorSchema } = require("../schemas");
 const ExpressError = require("../utilities/ExpressError");
-const { isLoggedIn } = require("../middleware");
+const { isLoggedIn, isAuthor } = require("../middleware");
 
 const validateSalon = (req, res, next) => {
   const { error } = salonValidatorSchema.validate(req.body);
@@ -50,47 +50,58 @@ router.get(
       .populate("salonAuthor");
     if (!salon) {
       req.flash("error", "Salon not found");
-      res.redirect("/salons");
+      res.redirect("/salons/index");
     } else {
       res.render("salons/show", { salon });
     }
   })
 );
 
+router.get(
+  "/:id/edit",
+  isLoggedIn,
+  isAuthor,
+  catchAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const salon = await Salon.findById(id);
+    if (!salon) {
+      req.flash("error", "Salon not found");
+      return res.redirect("/salons/index");
+    }
+    res.render("salons/edit", { salon });
+  })
+);
+
 router.put(
   "/:id",
   isLoggedIn,
+  isAuthor,
   validateSalon,
   catchAsync(async (req, res, next) => {
     const { id } = req.params;
-    const salon = await Salon.findByIdAndUpdate(id, { ...req.body.salon });
+    const salon = await Salon.findById(id);
+    const salonToUpdate = await Salon.findByIdAndUpdate(id, {
+      ...req.body.salon,
+    });
     req.flash("success", "Successfully edited salon info!");
-    res.redirect(`/salons/${salon._id}`);
+    res.redirect(`/salons/${salonToUpdate._id}`);
   })
 );
 
 router.delete(
   "/:id",
   isLoggedIn,
+  isAuthor,
   catchAsync(async (req, res, next) => {
     const { id } = req.params;
-    await Salon.findByIdAndDelete(id);
-    req.flash("success", "Successfully delete salon!");
-    res.redirect("/salons");
-  })
-);
-
-router.get(
-  "/:id/edit",
-  isLoggedIn,
-  catchAsync(async (req, res, next) => {
-    const salon = await Salon.findById(req.params.id);
+    const salon = await Salon.findById(id);
     if (!salon) {
       req.flash("error", "Salon not found");
-      res.redirect("/salons");
-    } else {
-      res.render("salons/edit", { salon });
+      return res.redirect("/salons/index");
     }
+    await Salon.findByIdAndDelete(id);
+    req.flash("success", "Successfully delete salon!");
+    res.redirect("/salons/index");
   })
 );
 
