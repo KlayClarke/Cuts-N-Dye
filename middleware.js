dc1fe779fb78ccaacf0f6a6a7f4ab5f67eac915f
Klay Clarke <klayaclarke@gmail.com>
2022-01-15 00:57:26 -0500

    add middleware to checks to see if user is the author of salon - if not, user is not allowed to access salon edit form, submit put request or delete request

--- middleware.js
+++ middleware.js
@@ -6,3 +6,13 @@ module.exports.isLoggedIn = (req, res, next) => {
   }
   next();
 };
+
+module.exports.isAuthor = async (req, res, next) => {
+  const { id } = req.params;
+  const salon = await Salon.findById(id);
+  if (!salon.salonAuthor.equals(req.user._id)) {
+    req.flash("error", "You do not have permission to access this route.");
+    return res.redirect(`/salons/${salon._id}`);
+  }
+  next();
+};
