33562f82af7ccb8dfb7268e8e1f7951850f91309
Klay Clarke <klayaclarke@gmail.com>
2022-01-16 02:51:06 -0500

    assign author to each review at time of creation(using current user id)

--- routes/reviews.js
+++ routes/reviews.js
@@ -12,6 +12,7 @@ router.post(
   catchAsync(async (req, res) => {
     const salon = await Salon.findById(req.params.id);
     const review = new Review(req.body.review);
+    review.author = req.user._id;
     salon.salonReviews.push(review);
     await review.save();
     await salon.save();
