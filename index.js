df4f8b4d9c0b3b542fc4d013d861d804ac9eccf5
Klay Clarke <klayaclarke@gmail.com>
2022-01-17 07:13:38 -0500

    import dotenv pkg - allows us to use environmental variables locally while in development mode

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
 const salonRoutes = require("./routes/salons");
 const reviewRoutes = require("./routes/reviews");
