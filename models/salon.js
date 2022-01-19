8bd137014d9fe1f25051ce4b16f6bbd2c52f12c0
Klay Clarke <klayaclarke@gmail.com>
2022-01-19 11:09:10 -0500

    add a virtual property (text to display on cluster map popup for each salon instance)

--- models/salon.js
+++ models/salon.js
@@ -2,36 +2,45 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const Review = require("./review");
 
-const SalonSchema = new Schema({
-  salonName: String,
-  salonType: String,
-  salonAveragePrice: Number,
-  salonImage: String,
-  salonStreetAddress: String,
-  salonCity: String,
-  salonState: String,
-  salonZipCode: String,
-  geometry: {
-    type: {
-      type: String,
-      enum: ["Point"],
-      required: true,
+const opts = { toJSON: { virtuals: true } };
+
+const SalonSchema = new Schema(
+  {
+    salonName: String,
+    salonType: String,
+    salonAveragePrice: Number,
+    salonImage: String,
+    salonStreetAddress: String,
+    salonCity: String,
+    salonState: String,
+    salonZipCode: String,
+    geometry: {
+      type: {
+        type: String,
+        enum: ["Point"],
+        required: true,
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
     },
-    coordinates: {
-      type: [Number],
-      required: true,
-    },
-  },
-  salonAuthor: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  },
-  salonReviews: [
-    {
+    salonAuthor: {
       type: Schema.Types.ObjectId,
-      ref: "Review",
+      ref: "User",
     },
-  ],
+    salonReviews: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Review",
+      },
+    ],
+  },
+  opts
+);
+
+SalonSchema.virtual("properties.mapPopupHTML").get(function () {
+  return `<h6><a href="/salons/${this._id}" class="remove-underline">${this.salonName}</a> ${this.salonCity}, ${this.salonState}</h6>`;
 });
 
 SalonSchema.post("findOneAndDelete", async function (salon) {
